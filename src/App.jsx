a2da0cc3f15b1f9650dc969a6390e4c5b4254a60
Junaid_Abbas_4545 <junaidabbasshah665@gmail.com>
2023-11-14 03:52:59 -0800

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import File from './Component/file'
+
+const App = () => {
+  return (
+    <File />
+  )
+}
+
+export default App
---
+++ src/Component/file.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const file = () => {
+  return (
+    <div className="text-center py-4">Hello</div>
+  )
+}
+
+export default file
